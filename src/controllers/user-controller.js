639f194c6f3df0ed2d1779b9adbf55f3a631308a
madhu <rayi.madhupriya@gmail.com>
2024-03-03 21:40:46 +0530

    Added token gen logic from sign in

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -25,6 +25,26 @@ const create= async(req,res)=>{
     }
 }
 
+const signIn= async(req,res)=>{
+    try{
+        const response= await userService.signIn(req.body.email,req.body.password);
+        return res.status(200).json({
+            data: response,
+            success: true,
+            message: "Successfully signed in",
+            err: {}
+        });
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong at sigin',
+            success: false,
+            err: error,
+            data:{}
+        })
+    }
+}
 module.exports ={
-    create
+    create,
+    signIn
 } 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -18,6 +18,25 @@ class UserService{
         }
     }
 
+    async signIn(email,plainPassword){
+        try{
+          //step-1:  fetch the user using the email
+          const user= await this.userRepository.getByEmail(email);
+          //step-2: compare incoming plain password with stores encrypted password
+          const passwordMatch= this.checkPassword(plainPassword,user.password);
+          if(!passwordMatch){
+                console.log("Password doesnt match");
+                throw {error: "Incorrect Password"};
+          }
+          //step-3: If password matches then create a token and send it to the user
+          const newJWT= this.createToken({email: user.email, id: user.id});
+          return newJWT;
+          
+        }catch(error){
+            console.log("Something went wrong in signin process");
+            throw error;
+        }
+    }
      createToken(user){
         try{
             const result=jwt.sign(user,JWT_KEY,{expiresIn: '1d'});
